ea46e97f10ce09f3d3fb20ab3c497eb4f1628905
TMaximoDeloitte03 <tmaximo@deloitte.pt>
2024-05-06 18:00:28 +0100

    Criei um controller, uma view e meti a route a chamar o controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\exibicao;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/exibicao',
+    [exibicao::class, 'exibicao']
+);

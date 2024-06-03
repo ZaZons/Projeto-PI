173c9f77d21d9e85a27b133665a1f2b94bb7c146
ZaZons <tiago.ogomes.2004@gmail.com>
2024-06-03 12:16:51 +0100

    Atualizar as routes para compilar

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,23 @@
 
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\exibicao;
-
-Route::get(
-    '/exibicao',
-    [exibicao::class, 'exibicao']
-);
 
 Route::get('/carrinho', function () {
     return view('carrinho');
 });
 
 Route::view('/', 'home')->name('root');
-Route::get('/home', HomeController::class, 'index')->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Auth::routes();
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
 
-Route::resource('clientes', ClienteController::class);
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/users/perfil', [ClienteController::class, 'show'])->name('perfil');
+
